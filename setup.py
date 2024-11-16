e609b5ff73703cab645e62f2645f0c2b686b3825
lwyh <lwyhnice@gmail.com>
2024-11-16 17:16:44 +0800

    file2data20241116

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# encoding:utf-8
+
+from setuptools import setup, find_packages
+ 
+setup(
+    name="file2data",
+    version="0.1",
+    license="MIT Licence",
+ 
+    url="https://github.com/yzbxTools/file2data",
+    author="lwyh",
+    author_email="lwyhnice@gmail.com",
+ 
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
